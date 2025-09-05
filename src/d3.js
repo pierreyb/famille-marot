56d4c447e413e7cd72f96efe5e0231d54a4f70c5
pierreyb <pierreyvesbernard@yahoo.com>
2025-09-05 12:11:18 +0200

    Update d3.js

--- src/d3.js
+++ src/d3.js
@@ -1,2 +1,3 @@
 //import * as _d3 from 'd3';  // comment this line to run locally with http-server
+import * as _d3 from 'https://unpkg.com/d3?module'
 export default typeof window === "object" && !!window.d3 ? window.d3 : _d3;
